5ec5cc9c4b975f1cc0f542ac7e77e746d59bf84a
Steven.Zhang <captain.cook.83@gmail.com>
2023-08-08 22:18:09 +0800

    -

--- Assets/Scripts/HandPose/HandSkeleton.cs
+++ Assets/Scripts/HandPose/HandSkeleton.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Cc83.HandPose
@@ -15,7 +16,8 @@ namespace Cc83.HandPose
         private const int FingerNodeCount = 3;
 
         private static readonly int[] SelectFingers = { 2, 3, 4 };
-        private static readonly int[] ActiveFingers = { 0, 1 };
+        private static readonly int[] ActiveFingers = { 1 };
+        private static readonly int[] ThumbFingers = { 0 };
 
         [Range(0.001f, 0.1f)]
         public float animateThreshold = 0.01f;
@@ -30,15 +32,67 @@ namespace Cc83.HandPose
 
         public HandPoseData defaultPoseData;
         public HandPoseData fistPoseData;
+
+        private InputDevice inputDevice;
         
         private float targetSelectValue;
         private float targetActiveValue;
+        private float  targetThumbValue;
 
         private float currentSelectValue;
         private float currentActiveValue;
+        private float currentThumbValue;
+
+        private void OnEnable()
+        {
+            InputDevices.deviceConnected += OnDeviceConnected;
+            InputDevices.deviceDisconnected  += OnDeviceDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            InputDevices.deviceConnected -= OnDeviceConnected;
+            InputDevices.deviceDisconnected -= OnDeviceDisconnected;
+        }
+
+        private void OnDeviceConnected(InputDevice device)
+        {
+            var inputDevices = new List<InputDevice>();
+            var deviceSide = handSide == HandSide.Left
+                ? InputDeviceCharacteristics.Left
+                : InputDeviceCharacteristics.Right;
+            
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.HeldInHand | deviceSide, inputDevices);
+            inputDevice = inputDevices.Count > 0 ? inputDevices[0] : default;
+        }
+
+        private void OnDeviceDisconnected(InputDevice device)
+        {
+            inputDevice = default;
+        }
 
         private void Update()
         {
+            targetThumbValue = 0;
+            
+            if (inputDevice != default)
+            {
+                if (inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out var secondaryButton) && secondaryButton)
+                {
+                    targetThumbValue = 1;
+                } 
+                else if (inputDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out var secondaryTouch) && secondaryTouch)
+                {
+                    targetThumbValue = 0.4f;
+                }
+            }
+            
+            if (Math.Abs(targetThumbValue - currentThumbValue) > animateThreshold)
+            {
+                currentThumbValue = Mathf.MoveTowards(currentThumbValue, targetThumbValue, Time.deltaTime * animateSpeed);
+                CalculateFingerNodes(ThumbFingers, currentThumbValue);
+            }
+            
             targetSelectValue = controller.selectAction.action.ReadValue<float>();
             if (Math.Abs(targetSelectValue - currentSelectValue) > animateThreshold)
             {

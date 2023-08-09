c5f7ba8f8449e1306f5ca45bbcabf7779e3a4174
Steven.Zhang <captain.cook.83@gmail.com>
2023-08-10 00:34:45 +0800

    添加枪支射击时自身动画效果

--- Assets/Scripts/Interactable/ShootController.cs
+++ Assets/Scripts/Interactable/ShootController.cs
@@ -3,16 +3,27 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Cc83.Interactable
 {
+    [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(AudioSource))]
     [RequireComponent(typeof(XRGrabInteractable))]
     public class ShootController : MonoBehaviour
     {
+        private static readonly int Shoot = Animator.StringToHash("Shoot");
+
+        [Range(0.01f, 0.5f)]
+        public float cdTime = 0.17f;
+        
+        private Animator animator;
+        
         private AudioSource audioSource;
         
         private XRGrabInteractable interactable;
 
+        private float triggerTime;
+
         private void Awake()
         {
+            animator = GetComponent<Animator>();
             audioSource = GetComponent<AudioSource>();
             interactable = GetComponent<XRGrabInteractable>();
             interactable.activated.AddListener(OnShootActive);
@@ -25,7 +36,16 @@ namespace Cc83.Interactable
 
         private void OnShootActive(ActivateEventArgs args)
         {
+            var currentTime = Time.time;
+            if (currentTime < triggerTime)
+            {
+                return;
+            }
+
+            triggerTime = currentTime + cdTime;
+            
             audioSource.Play();
+            animator.SetTrigger(Shoot);
         }
     }
 }

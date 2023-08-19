1bc778f1ceb6dac8c4688b4ee1a05e73c804af38
Steven.Zhang <captain.cook.83@gmail.com>
2023-08-19 21:37:52 +0800

    优化角色控制器

--- Assets/Scripts/Character/Animator Controller/StopBehaviourAnimator.cs
+++ Assets/Scripts/Character/Animator Controller/StopBehaviourAnimator.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Cc83.Character.Behaviour
 {
+    [SharedBetweenAnimators]
     public class StopBehaviourAnimator : StateMachineBehaviour
     {
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

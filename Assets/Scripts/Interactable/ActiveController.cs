16cc5b6d02cc40b4c69f5e12c55dcddec803f509
Steven.Zhang <captain.cook.83@gmail.com>
2023-09-11 20:40:51 +0800

    添加爆炸视觉效果

--- Assets/Scripts/Interactable/ActiveController.cs
+++ Assets/Scripts/Interactable/ActiveController.cs
@@ -1,4 +1,4 @@
-﻿using Cc83.Character;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -6,35 +6,73 @@ namespace Cc83.Interactable
 {
     public class ActiveController : MonoBehaviour
     {
-        private XRGrabInteractable _interactable;
+        [SerializeField]
+        private XRGrabInteractable interactable;
         
-        private HandController _handController;
+        [SerializeField]
+        private GameObject explodeEffect;
+        
+        [SerializeField]
+        [Range(0, 5)]
+        private float explodeDelayAfterCollision = 1f;
+
+        [SerializeField]
+        [Range(3, 5)]
+        private float explodeDelayBeforeThrow = 4.5f;
+
+        private bool _explodeOnCollision;
+        
+        private Coroutine _pendingExplosion;
         
         private void Awake()
         {
-            _interactable = GetComponent<XRGrabInteractable>();
-            
-            _interactable.activated.AddListener(OnShootActive);
-            _interactable.deactivated.AddListener(OnShootDeactivate);
+            interactable.deactivated.AddListener(OnDeactivate);
+            interactable.selectExited.AddListener(OnDeselected);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (_explodeOnCollision)
+            {
+                _pendingExplosion = StartCoroutine(ExplodeEffect(explodeDelayAfterCollision));
+            }
         }
 
         private void OnDestroy()
         {
-            _interactable.activated.RemoveListener(OnShootActive);
-            _interactable.deactivated.RemoveListener(OnShootDeactivate);
-        }
+            interactable.deactivated.RemoveListener(OnDeactivate);
+            interactable.selectExited.RemoveListener(OnDeselected);
 
-        private void OnShootActive(ActivateEventArgs args)
-        {
-            _handController = args.interactorObject.transform.GetComponentInParent<HandController>();
-        }
-
-        private void OnShootDeactivate(DeactivateEventArgs args)
-        {
-            if (_handController)
+            if (_pendingExplosion != null)
             {
-                _handController = null;
+                StopCoroutine(_pendingExplosion);
             }
+        }
+
+        private void OnDeselected(SelectExitEventArgs args)
+        {
+            if (_pendingExplosion != null)
+            {
+                StopCoroutine(_pendingExplosion);
+                
+                _pendingExplosion = null;
+                _explodeOnCollision = true;
+            }
+        }
+
+        private void OnDeactivate(DeactivateEventArgs args)
+        {
+            _pendingExplosion = StartCoroutine(ExplodeEffect(explodeDelayBeforeThrow));
+        }
+
+        private IEnumerator ExplodeEffect(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            
+            _pendingExplosion = null;
+            
+            Destroy(Instantiate(explodeEffect, transform.position, Quaternion.identity), 2);
+            Destroy(gameObject);
         }
     }
 }

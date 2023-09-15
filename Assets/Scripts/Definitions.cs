8368b1e2e3f92326e3f541d385327a2d6d5956c2
Steven.Zhang <captain.cook.83@gmail.com>
2023-09-15 09:05:01 +0800

    修复射击时障碍物层级检测丢失 BUG

--- Assets/Scripts/Definitions.cs
+++ Assets/Scripts/Definitions.cs
@@ -10,12 +10,14 @@ namespace Cc83
         
         public static readonly int DefaultLayer = LayerMask.NameToLayer("Default");
         
+        public static readonly int ObstacleLayer = LayerMask.NameToLayer("Obstacle");
+        
         public static readonly int DynamicLayer = LayerMask.NameToLayer("Dynamic");
         
         public static readonly int CharacterLayer = LayerMask.NameToLayer("Character");
         
         public static readonly int WeaponLayer = LayerMask.NameToLayer("Weapon");
         
-        public static readonly LayerMask ShootTargetLayerMask = (1 << DefaultLayer) | (1 << DynamicLayer) | (1 << CharacterLayer) | (1 << WeaponLayer);
+        public static readonly LayerMask ShootTargetLayerMask = (1 << DefaultLayer) | (1 << ObstacleLayer) | (1 << DynamicLayer) | (1 << CharacterLayer) | (1 << WeaponLayer);
     }
 }

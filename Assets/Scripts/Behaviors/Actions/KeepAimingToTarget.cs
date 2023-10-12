d82930881026f015344b0795f7b3bcdc51c47991
Steven.Zhang <captain.cook.83@gmail.com>
2023-10-12 23:33:06 +0800

    射击旋转过程中，枪口保持对准目标

---
+++ Assets/Scripts/Behaviors/Actions/KeepAimingToTarget.cs
@@ -0,0 +1,23 @@
+using BehaviorDesigner.Runtime.Tasks;
+using Cc83.Character;
+
+namespace Cc83.Behaviors
+{
+    [TaskCategory("Cc83")]
+    public class KeepAimingToTarget : Action
+    {
+        private EnemyAttackController _attackController;
+
+        public override void OnAwake()
+        {
+            _attackController = GetComponent<EnemyAttackController>();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            _attackController.TickAiming();
+            
+            return TaskStatus.Running;
+        }
+    }
+}
--- Assets/Scripts/Character/NPC/EnemyAttackController.cs
+++ Assets/Scripts/Character/NPC/EnemyAttackController.cs
@@ -73,25 +73,6 @@ namespace Cc83.Character
                         if (rotationAngle > RightRetargetAngle) return Mathf.CeilToInt(dotDirection);
                         break;
                 }
-
-                var currentHitPosition = _sensorTarget.targetAgent.HitPosition;
-                var aimingDeviation = Vector3.SqrMagnitude(aimingTowards.position - currentHitPosition);
-                if (aimingDeviation > 0.01f)       // 0.1m
-                {
-                    _aimingTarget = currentHitPosition;
-                }
-            }
-
-            var aimingPosition = aimingTowards.position;
-            if (!aimingPosition.Equals(_aimingTarget))
-            {
-                var position = Vector3.Lerp(aimingPosition, _aimingTarget, Time.deltaTime * 5f);            // TODO 旋转速度需要依据旋转量，不应该使用固定速度
-                aimingTowards.position = position;
-                
-                if (Vector3.SqrMagnitude(position - _aimingTarget) < 0.01f)
-                {
-                    _aimingTarget = position;                       // 此处并非像常规差值结果一样执行 aimingTowards.position = _aimingTarget，而是反过来修改原始目标值 _aimingTarget，这样来营造射击时重新瞄准造成的偏差
-                }
             }
             
             var currentTime = Time.time;
@@ -103,13 +84,34 @@ namespace Cc83.Character
                 _nextShootTime = currentTime + duration + Random.Range(0.5f, _maxRepeatShootDelay);
                 _shootController.Shoot(times);
             }
+            
+            TickAiming();
 
             return 0;
         }
-        
-        public void Deactive()
+
+        public void TickAiming()
         {
-            _sensorTarget = null;
+            var currentHitPosition = _sensorTarget.targetAgent.HitPosition;
+            var aimingDeviation = Vector3.SqrMagnitude(aimingTowards.position - currentHitPosition);
+            if (aimingDeviation > 0.01f)       // 0.1m
+            {
+                _aimingTarget = currentHitPosition;
+            }
+            
+            var aimingPosition = aimingTowards.position;
+            if (!aimingPosition.Equals(_aimingTarget))
+            {
+                var position = Vector3.Lerp(aimingPosition, _aimingTarget, Time.deltaTime * 15f);            // TODO 旋转速度需要依据旋转量，不应该使用固定速度
+                aimingTowards.position = position;
+                
+                if (Vector3.SqrMagnitude(position - _aimingTarget) < 0.01f)
+                {
+                    _aimingTarget = position;                       // 此处并非像常规差值结果一样执行 aimingTowards.position = _aimingTarget，而是反过来修改原始目标值 _aimingTarget，这样来营造射击时重新瞄准造成的偏差
+                }
+            }
         }
+        
+        public void Deactive() { }
     }
 }

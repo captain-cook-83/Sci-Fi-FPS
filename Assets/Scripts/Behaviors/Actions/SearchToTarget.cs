79888eefec47e950da388ad93b31148a9eed692a
Steven.Zhang <captain.cook.83@gmail.com>
2023-10-16 23:51:30 +0800

    -

--- Assets/Scripts/Behaviors/Actions/SearchToTarget.cs
+++ Assets/Scripts/Behaviors/Actions/SearchToTarget.cs
@@ -1,7 +1,9 @@
-﻿namespace Cc83.Behaviors
+﻿using UnityEngine;
+
+namespace Cc83.Behaviors
 {
     public class SearchToTarget : MoveToTarget
     {
-        protected override float Speed => 1.8f;
+        protected override float Speed => Random.Range(2.5f, 4f);
     }
 }
